2313e26f8def09a5d73e7c02e40d0d0a9bad0403
im <imartemy1524@gmail.com>
2025-04-25 20:45:05 +0200

    1 working

--- problem1/public1.spec.ts
+++ problem1/public1.spec.ts
@@ -10,7 +10,7 @@ it('solution1', async () => {
     const proposal = blockchain.openContract(
         await Proposal.fromInit({
             $$type: 'Init',
-            proposalId: 0n,
+            proposalId: 1234567n,
             votingEndingAt: BigInt(Math.floor(Date.now() / 1000)) + 24n * 60n * 60n,
         }),
     );
@@ -25,9 +25,12 @@ it('solution1', async () => {
         null, // empty message, handled by `receive()` without parameters
     );
 
+    // vote
+
+
     for (let i = 0; i < 50; i++) {
         // vote
-        const voter = await blockchain.treasury('v' + i);
+        const voter = await blockchain.treasury('voter' + i);
         const { transactions } = await proposal.send(
             voter.getSender(),
             { value: toNano('0.1') },
@@ -36,12 +39,11 @@ it('solution1', async () => {
                 value: true,
             },
         );
-        printTransactionFees(transactions);
-        expect(transactions).not.toHaveTransaction({
-            exitCode: i=>i != 0
-        });
-        printTransactionFees(transactions)
-        if (i != 49)
+        console.log(fromNano(transactions[1].totalFees.coins));
+        expect(transactions).toHaveTransaction({
+            to: voter.getSender().address
+        })
+        if(i != 49)
             await proposal.send(
                 (await blockchain.treasury('avoter' + i)).getSender(),
                 { value: toNano('0.1') },
@@ -50,16 +52,10 @@ it('solution1', async () => {
                     value: false,
                 },
             );
-        else console.log(fromNano(transactions[1].totalFees.coins));
     }
+    const voter = await blockchain.treasury('voter0');
 
-
-
-    const state = await proposal.getProposalState();
-    // the vote was counted
-    expect(state).toMatchObject({ yesCount: 50n, noCount: 49n });
-    const voter = await blockchain.treasury('v' + 2);
-    const { transactions } = await proposal.send(
+    await proposal.send(
         voter.getSender(),
         { value: toNano('0.1') },
         {
@@ -67,5 +63,27 @@ it('solution1', async () => {
             value: true,
         },
     );
-    expect(await proposal.getProposalState()).toMatchObject({ yesCount: 50n, noCount: 49n });
+    //     console.log(fromNano(tr2[1].totalFees.coins));
+    const state = await proposal.getProposalState();
+    // the vote was counted
+    expect(state).toMatchObject({ yesCount: 50n, noCount: 49n });
+    await proposal.send(
+        (await blockchain.treasury("111")).getSender(),
+        { value: toNano('0.1') },
+        {
+            $$type: 'Vote',
+            value: true,
+        },
+    );
+    await proposal.send(
+        (await blockchain.treasury("222")).getSender(),
+        { value: toNano('0.1') },
+        {
+            $$type: 'Vote',
+            value: true,
+        },
+    );
+    expect(await proposal.getProposalState()).toMatchObject({ yesCount: 51n, noCount: 49n });
+
 });
+
