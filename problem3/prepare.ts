6bdab8dbd4901ccf95178124f1b9dcd0f2023e0b
im <imartemy1524@gmail.com>
2025-04-25 02:21:53 +0200

    aaaa, working

--- problem3/prepare.ts
+++ problem3/prepare.ts
@@ -2,68 +2,38 @@ import { compileFunc } from '@ton-community/func-js';
 import fs from 'fs';
 import { Cell } from '@ton/core';
 
-
-async function compileChild(){
+async function compileParent() {
     const code = await compileFunc({
         sources: [
             {
-                filename: "index.fc",
-                content: await fs.promises.readFile(__dirname +"/child_code.fc", "utf-8"),
+                filename: 'code.fc',
+                content: await fs.promises.readFile(__dirname + '/code.fc', 'utf-8'),
             },
             {
-                filename: "imports/stdlib.fc",
-                content: await fs.promises.readFile(__dirname +"/imports/stdlib.fc", "utf-8"),
-            }
-        ]
-    })
-    if(code.status !== "ok") {
-        console.log(code);
-        throw new Error("Compilation failed");
-    }
-    const cellCode = Cell.fromBase64(code.codeBoc);
-    return cellCode;
-}
-async function writeCodeToFile(code: Cell){
-    const string = code.toBoc().toString("hex")
-    const codeString = `cell get_child_code() asm "B{${string}} B>boc PUSHREF";\n`
-    await fs.promises.rm(__dirname + "/_compiled_code.fc");
-    await fs.promises.writeFile(__dirname + "/_compiled_code.fc", codeString);
-}
-async function compileParent(){
-    const code = await compileFunc({
-        sources: [
-            {
-                filename: "code.fc",
-                content: await fs.promises.readFile(__dirname +"/code.fc", "utf-8"),
+                filename: 'imports/stdlib.fc',
+                content: await fs.promises.readFile(__dirname + '/imports/stdlib.fc', 'utf-8'),
             },
-            {
-                filename: "imports/stdlib.fc",
-                content: await fs.promises.readFile(__dirname +"/imports/stdlib.fc", "utf-8"),
-            },
-            {
-                filename: "_compiled_code.fc",
-                content: await fs.promises.readFile(__dirname +"/_compiled_code.fc", "utf-8"),
-            }
-        ]
-    })
-    if(code.status !== "ok") {
+        ],
+    });
+    if (code.status !== 'ok') {
         console.error(code.message);
-        throw new Error("Compilation failed");
+        throw new Error('Compilation failed');
     }
     const cellCode = Cell.fromBase64(code.codeBoc);
     return cellCode;
 }
+
 async function replaceConstantInTactFile(codeToReplace: Cell) {
-    const code = codeToReplace.toBoc().toString("hex");
+    const code = codeToReplace.toBoc().toString('hex');
     try {
         // Read the file asynchronously
-        const filePath = 'solution3.tact';
+        const filePath = __dirname + '/solution3.tact';
         let content = await fs.promises.readFile(filePath, 'utf-8');
 
         // Replace the blablabla with constant f
         const updatedContent = content.replace(
             /asm fun actual_code\(\): Cell \{ B\{.+\} B>boc PUSHREF }/,
-            `asm fun actual_code(): Cell { B{${code}} B>boc PUSHREF }`
+            `asm fun actual_code(): Cell { B{${code}} B>boc PUSHREF }`,
         );
 
         // Write the changes back to the file
@@ -75,12 +45,10 @@ async function replaceConstantInTactFile(codeToReplace: Cell) {
     }
 }
 
-
-async function main(){
-    const code = await compileChild();
-    await writeCodeToFile(code);
+async function main() {
     const parentCode = await compileParent();
     replaceConstantInTactFile(parentCode);
-// in file solution3.tact replace asm fun actual_code(): Cell { B{blablabla} B>boc PUSHREF } with code
+    // in file solution3.tact replace asm fun actual_code(): Cell { B{blablabla} B>boc PUSHREF } with code
 }
-main()
+
+main();
